3f86ed42723ec9feba42142648aaf9b853269ce0
Søren Østergaard <soestergaards@gmail.com>
2016-04-02 14:49:39 +0200

    expand score check functionality

--- src/utils/score.js
+++ src/utils/score.js
@@ -1,5 +1,27 @@
-export function ones(arr) {
+export function sum(arr) {
+  return arr.reduce((prev, cur) => prev + cur, 0);
+}
+
+export function kind(arr, type) {
+  return sum(arr.filter(score => score === type));
+}
+
+export function pair(arr, amount, type) {
+  if (type) {
+    return kind(arr, type) / type >= amount ? type * amount : 0;
+  }
+
   return arr
-    .filter(score => score === 1)
-    .reduce((ps, cs) => ps + cs);
+    .map(n => kind(arr, n) / n >= amount ? kind(arr, n) : 0)
+    .filter(n => n)
+    .reduce((prev, cur) => prev > cur ? prev : cur, 0);
+}
+
+export function pairs(arr, amount, ofAmount) {
+  return arr
+    .map((n, i) => pair(arr, amount, i + 1))
+    .filter(n => n)
+    .reverse()
+    .slice(undefined, ofAmount)
+    .reduce((prev, cur) => prev + cur, 0);
 }
--- tests/utils_test.js
+++ tests/utils_test.js
@@ -1,8 +1,53 @@
 import { assert } from 'chai';
-import { ones } from '../src/utils/score';
+import { sum, kind, pair, pairs } from '../src/utils/score';
 
-describe('Score', () => {
-  it('Calculating ones given 4 ones results in 4', () => {
-    assert.equal(ones([2, 2, 1, 1, 1, 1]), 4);
+describe('Score:', () => {
+  it('sum returns the sum of an array of numbers', () => {
+    assert.equal(sum([3, 1, 5, 6, 4, 1]), 20);
+  });
+
+  it('kind returns the sum of x kind', () => {
+    assert.equal(kind([3, 1, 5, 5, 5, 5], 5), 20);
+    assert.equal(kind([2, 2, 1, 4, 5, 3], 2), 4);
+  });
+
+  it('pair returns a pair of twos', () => {
+    assert.equal(pair([3, 2, 2, 6, 4, 1], 2), 4);
+  });
+
+  it('pair returns a pair of ones', () => {
+    assert.equal(pair([3, 1, 2, 6, 4, 1], 2), 2);
+  });
+
+  it('pair returns 10 if a 2-pair of 5s exists', () => {
+    assert.equal(pair([3, 5, 5, 6, 4, 1], 2, 5), 10);
+  });
+
+  it('pair returns the highest 3-pair', () => {
+    assert.equal(pair([3, 3, 3, 4, 4, 4], 3), 12);
+  });
+
+  it('pair returns 0 if no 3-pair exists', () => {
+    assert.equal(pair([3, 2, 3, 6, 4, 4], 3), 0);
+  });
+
+  it('pair returns 0 if no 2-pair of 5s', () => {
+    assert.equal(pair([3, 5, 6, 6, 4, 1], 2, 5), 0);
+  });
+
+  it('pairs returns 14 if there\'s a pair of 3s and 4s', () => {
+    assert.equal(pairs([3, 3, 4, 6, 4, 1], 2, 2), 14);
+  });
+
+  it('pairs returns 16 if there\'s a pair of 1s, 3s and 4s', () => {
+    assert.equal(pairs([3, 3, 4, 1, 4, 1], 2, 3), 16);
+  });
+
+  it('pairs returns 21 if there\'s a 3-pair of 2s and 5s', () => {
+    assert.equal(pairs([2, 2, 5, 5, 5, 2], 3, 2), 21);
+  });
+
+  it('pairs returns 20 if there\'s a pair of 3s and 6s, even though there\'s a pair of 2s as well', () => {
+    assert.equal(pairs([6, 2, 4, 6, 4, 2], 2, 2), 20);
   });
 });
