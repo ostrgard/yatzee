e7da06dc771dc81a16ad807b7db6f57acaabf6b4
Søren Østergaard <soestergaards@gmail.com>
2016-04-03 11:47:44 +0200

    finished score util

--- src/utils/score.js
+++ src/utils/score.js
@@ -2,26 +2,87 @@ export function sum(arr) {
   return arr.reduce((prev, cur) => prev + cur, 0);
 }
 
-export function kind(arr, type) {
-  return sum(arr.filter(score => score === type));
+export function kind(arr, ofType) {
+  const hits = arr.filter(score => score === ofType);
+
+  return {
+    score: sum(arr.filter(score => score === ofType)),
+    occurrences: hits.length
+  };
 }
 
-export function pair(arr, amount, type) {
-  if (type) {
-    return kind(arr, type) / type >= amount ? type * amount : 0;
+export function pair(arr, amount, ofType, notOfType) {
+  if (ofType) {
+    return {
+      score: kind(arr, ofType).occurrences >= amount ? ofType * amount : 0,
+      type: ofType
+    };
   }
 
   return arr
-    .map(n => kind(arr, n) / n >= amount ? kind(arr, n) : 0)
-    .filter(n => n)
-    .reduce((prev, cur) => prev > cur ? prev : cur, 0);
+    .map(n => {
+      return {
+        score: kind(arr, n).occurrences >= amount ? n * amount : 0,
+        type: n
+      };
+    })
+    .filter(n => n.score && (!notOfType || notOfType !== n.type))
+    .reduce((prev, cur) => prev.score > cur.score ? prev : cur, {score: 0, type: undefined});
 }
 
 export function pairs(arr, amount, ofAmount) {
   return arr
     .map((n, i) => pair(arr, amount, i + 1))
-    .filter(n => n)
+    .filter(n => n.score)
     .reverse()
     .slice(undefined, ofAmount)
-    .reduce((prev, cur) => prev + cur, 0);
+    .reduce((prev, cur) => {
+      return {
+        score: prev.score + cur.score,
+        type: prev.type ? prev.type.concat([cur.type]) : [cur.type]
+      };
+    }, {score: 0, type: undefined});
+}
+
+export function house(arr) {
+  const pair3 = pair(arr, 3);
+  const pair2 = pair(arr, 2, undefined, pair3.type);
+
+  if (!pair3.score || !pair2.score) {
+    return {
+      score: 0,
+      type: undefined
+    };
+  }
+
+  return {
+    score: pair3.score + pair2.score,
+    type: [pair3.type, pair2.type]
+  };
+}
+
+function findAll(arr, all) {
+  for (let i = 0; i < all.length; i++) {
+    if (!arr.find(n => n === all[i])) { // eslint-disable-line no-loop-func
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function straight(arr, type) {
+  if (type === 'royal' && findAll(arr, [1, 2, 3, 4, 5, 6])) {
+    return 21;
+  }
+
+  if (type === 'small' && findAll(arr, [1, 2, 3, 4, 5])) {
+    return 15;
+  }
+
+  if (type === 'large' && findAll(arr, [2, 3, 4, 5, 6])) {
+    return 20;
+  }
+
+  return 0;
 }
--- tests/utils_test.js
+++ tests/utils_test.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { sum, kind, pair, pairs } from '../src/utils/score';
+import { sum, kind, pair, pairs, house, straight } from '../src/utils/score';
 
 describe('Score:', () => {
   it('sum returns the sum of an array of numbers', () => {
@@ -7,47 +7,86 @@ describe('Score:', () => {
   });
 
   it('kind returns the sum of x kind', () => {
-    assert.equal(kind([3, 1, 5, 5, 5, 5], 5), 20);
-    assert.equal(kind([2, 2, 1, 4, 5, 3], 2), 4);
+    assert.deepEqual(kind([3, 1, 5, 5, 5, 5], 5), {score: 20, occurrences: 4});
   });
 
   it('pair returns a pair of twos', () => {
-    assert.equal(pair([3, 2, 2, 6, 4, 1], 2), 4);
+    assert.deepEqual(pair([3, 2, 2, 6, 4, 1], 2), {score: 4, type: 2});
   });
 
   it('pair returns a pair of ones', () => {
-    assert.equal(pair([3, 1, 2, 6, 4, 1], 2), 2);
+    assert.deepEqual(pair([3, 1, 2, 6, 4, 1], 2), {score: 2, type: 1});
   });
 
   it('pair returns 10 if a 2-pair of 5s exists', () => {
-    assert.equal(pair([3, 5, 5, 6, 4, 1], 2, 5), 10);
+    assert.deepEqual(pair([3, 5, 5, 6, 4, 1], 2, 5), {score: 10, type: 5});
+  });
+
+  it('pair returns 10 if a 2-pair of 5s exists, even though there\'s more 5s', () => {
+    assert.deepEqual(pair([3, 5, 5, 5, 5, 5], 2, 5), {score: 10, type: 5});
+  });
+
+  it('pair returns 4 if a 2-pair of 2s exists alongside a 2-pair of 4s, which is passed as notOfType param', () => {
+    assert.deepEqual(pair([4, 2, 4, 3, 6, 2], 2, undefined, 4), {score: 4, type: 2});
   });
 
   it('pair returns the highest 3-pair', () => {
-    assert.equal(pair([3, 3, 3, 4, 4, 4], 3), 12);
+    assert.deepEqual(pair([3, 3, 3, 4, 4, 4], 3), {score: 12, type: 4});
   });
 
   it('pair returns 0 if no 3-pair exists', () => {
-    assert.equal(pair([3, 2, 3, 6, 4, 4], 3), 0);
+    assert.deepEqual(pair([3, 2, 3, 6, 4, 4], 3), {score: 0, type: undefined});
   });
 
   it('pair returns 0 if no 2-pair of 5s', () => {
-    assert.equal(pair([3, 5, 6, 6, 4, 1], 2, 5), 0);
+    assert.deepEqual(pair([3, 5, 6, 6, 4, 1], 2, 5), {score: 0, type: 5});
   });
 
   it('pairs returns 14 if there\'s a pair of 3s and 4s', () => {
-    assert.equal(pairs([3, 3, 4, 6, 4, 1], 2, 2), 14);
+    assert.deepEqual(pairs([3, 3, 4, 6, 4, 1], 2, 2), {score: 14, type: [4, 3]});
   });
 
   it('pairs returns 16 if there\'s a pair of 1s, 3s and 4s', () => {
-    assert.equal(pairs([3, 3, 4, 1, 4, 1], 2, 3), 16);
+    assert.deepEqual(pairs([3, 3, 4, 1, 4, 1], 2, 3), {score: 16, type: [4, 3, 1]});
   });
 
   it('pairs returns 21 if there\'s a 3-pair of 2s and 5s', () => {
-    assert.equal(pairs([2, 2, 5, 5, 5, 2], 3, 2), 21);
+    assert.deepEqual(pairs([2, 2, 5, 5, 5, 2], 3, 2), {score: 21, type: [5, 2]});
   });
 
-  it('pairs returns 20 if there\'s a pair of 3s and 6s, even though there\'s a pair of 2s as well', () => {
-    assert.equal(pairs([6, 2, 4, 6, 4, 2], 2, 2), 20);
+  it('pairs returns 20 if there\'s a pair of 4s and 6s, even though there\'s a pair of 2s as well', () => {
+    assert.deepEqual(pairs([6, 2, 4, 6, 4, 2], 2, 2), {score: 20, type: [6, 4]});
+  });
+
+  it('house returns score if there\'s a 2-pair and a 3-pair', () => {
+    assert.deepEqual(house([3, 3, 4, 4, 4, 2]), {score: 18, type: [4, 3]});
+  });
+
+  it('house uses the highest number for the 3-pair, if there\'s two 3-pairs', () => {
+    assert.deepEqual(house([3, 3, 4, 4, 4, 3]), {score: 18, type: [4, 3]});
+  });
+
+  it('house returns 0 if there\'s no 2-pair but a 3-pair', () => {
+    assert.deepEqual(house([3, 2, 4, 4, 4, 1]), {score: 0, type: undefined});
+  });
+
+  it('house returns 0 if there\'s neither a 2-pair or a 3-pair', () => {
+    assert.deepEqual(house([3, 2, 4, 6, 5, 1]), {score: 0, type: undefined});
+  });
+
+  it('straight returns 21 if there\'s one of each and royal is passed as type', () => {
+    assert.equal(straight([3, 5, 4, 6, 2, 1], 'royal'), 21);
+  });
+
+  it('straight returns 0 if there\'s not one of each and royal is passed as type', () => {
+    assert.equal(straight([3, 5, 4, 6, 2, 1], 'royal'), 21);
+  });
+
+  it('straight return 15 on small straight', () => {
+    assert.equal(straight([5, 1, 4, 3, 1, 2], 'small'), 15);
+  });
+
+  it('straight return 20 on large straight', () => {
+    assert.equal(straight([6, 3, 2, 6, 5, 4], 'large'), 20);
   });
 });
